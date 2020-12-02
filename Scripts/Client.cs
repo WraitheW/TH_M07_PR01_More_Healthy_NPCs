ea95ee717b4b0ad7190dca5f5ac7289b9e0f3675
WraitheW <48034189+WraitheW@users.noreply.github.com>
2020-12-02 12:17:03 -0700

    Add files via upload

---
+++ Scripts/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public GameObject[] npc1;
+    public GameObject[] npc2;
+    public GameObject[] npc3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        npc1 = GameObject.FindGameObjectsWithTag("NPC1");
+        npc2 = GameObject.FindGameObjectsWithTag("NPC2");
+        npc3 = GameObject.FindGameObjectsWithTag("NPC3");
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            foreach (GameObject go in npc2)
+            {
+                go.GetComponent<NPC>().TakeDamage(10);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            foreach (GameObject go in npc1)
+            {
+                go.GetComponent<NPC>().TakeDamage(5);
+            } 
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            foreach (GameObject go in npc3)
+            {
+                go.GetComponent<NPC>().TakeDamage(0);
+            }
+        }
+    }
+}
---
+++ Scripts/Creation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Creation : MonoBehaviour
+{
+    public GameObject NPC1;
+    public GameObject NPC2;
+    public GameObject NPC3;
+
+    public Transform spawner;
+
+    public Button npc1;
+    public Button npc2;
+    public Button npc3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        npc1.onClick.AddListener(delegate { Create(NPC1); });
+        npc2.onClick.AddListener(delegate { Create(NPC2); });
+        npc3.onClick.AddListener(delegate { Create(NPC3); });
+    }
+
+    void Create(GameObject name)
+    {
+        GameObject go = Instantiate(name) as GameObject;
+        go.transform.position = spawner.position;
+    }
+}
